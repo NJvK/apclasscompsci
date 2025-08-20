0455febeea2e7f0aea3be8e08db18f37434ef12b
NJvK <vonkrogh.natalie@gmail.com>
2025-08-20 14:06:53 -0700

    offically finnished classwork

---
+++ 0820/Middle.java
@@ -0,0 +1,19 @@
+public class Middle {
+    public static void main (String [] args){
+        int numOne = 5;
+        int numTwo = 10;
+        System.out.println(numOne + numTwo);
+
+        int middle1 = (numOne + numTwo) /2;
+        System.out.println(middle1);
+
+        double middle2 = (numOne + numTwo) /2.0;
+        System.out.println(middle2);
+
+        double double1 = 3.0;
+        double double2 = 3.9;
+        double double3 = 4.8;
+        double average = (double1 + double2 + double3) /3.0;
+        System.out.println(average);
+    }
+}
---
+++ 0820/Test2.java
@@ -0,0 +1,38 @@
+public class Test2 {
+    public static void main (String [] args){
+        // int variables
+        int numOne = 5;
+        int numTwo = 10;
+        System.out.println(numOne + " " + numTwo);
+
+        // float variables
+        float floatOne = 5.0f;
+        float floatTwo = 10.0f;
+        System.out.println(floatOne + " " + floatTwo);
+
+        // char variables
+        char charOne = 'A';
+        char charTwo = 'B';
+        System.out.println(charOne + " " + charTwo);
+
+        // double variables
+        double doubleOne = 5.10;
+        double doubleTwo = 10.5;
+        System.out.println(doubleOne + " " + doubleTwo);
+
+        // boolean variables
+        boolean boolOne = true;
+        boolean boolTwo = false;
+        System.out.println(boolOne + " " + boolTwo);
+
+        // result
+        int resultOne = numOne + numTwo;
+        System.out.println(resultOne);
+
+        float resultTwo = floatOne + floatTwo;
+        System.out.println(resultTwo);
+
+        double resultThree = doubleOne + doubleTwo;
+        System.out.println(resultThree);
+    }
+}
