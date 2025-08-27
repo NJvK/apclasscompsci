7cc97e2a73f9c0cdc4bba88248937f7ab15dceeb
NJvK <vonkrogh.natalie@gmail.com>
2025-08-27 10:47:07 -0700

    Almost done

---
+++ 0827/Birthday.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Birthday {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double year, month;
+
+        System.out.println("Enter in the year you were born ");
+        year = scan.nextDouble();
+
+        System.out.println("Enter in the month you were born ");
+        month = scan.nextDouble();
+
+        if (year > 2010)
+    }
+}
