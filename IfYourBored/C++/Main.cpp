c8373f2a3819a46b2ca7592f19a74bd8b6b30d8b
NJvK <vonkrogh.natalie@gmail.com>
2025-12-15 14:36:13 -0800

    working on C++ totorial

---
+++ IfYourBored/C++/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    // int age = 14;
+    // double gpa = 4.2;
+    // char grade = 'A';
+    // bool isStudent = true;
+    // std::string name = "Natalie";
+    // ---------------------------------
+    // std::cout << "Enter in your age";
+    // int age;
+    // std::cin >> age;
+    std::string name;
+    getline(std::cin, name);
+}
